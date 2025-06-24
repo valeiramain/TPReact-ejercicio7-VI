b38babaf43e37aa147d69b9692d6426d68117df7
Valentina Iramain <valentinairamain2020@gmail.com>
2025-06-24 09:44:26 -0300

    se contiua maquetado de card

--- src/components/EmpleadoRow.jsx
+++ src/components/EmpleadoRow.jsx
@@ -1,27 +1,26 @@
-import {Card} from 'react-bootstrap';
+import { Card, Badge } from 'react-bootstrap';
 
 const EmpleadoRow = (props) => {
 
     console.log(props.valor)
     return (
         // continuacion sistema de grillas
-        <Card className="my-2 border border-dark">
+        <Card className="my-2 border border-light bg-light-subtle">
             <Card.Body>
-            <div className='row'>
-                <div className='col-sm-12 col-md-4'>
-                    {/* columna para la imagen del personaje */}
-                    
-                    {/* <img src={props.valor.pic} className="card-img-top" alt={props.valor.fullName}></img> */}
-                </div>
-                <div className='col-sm-12 col-md-8'>
-                    {/* columna para la frase del personaje */}
-                    <Card.Title>{props.valor.fullName}</Card.Title>
-                    <Card.Text>
-                    <p>{props.valor.title} <span className="card-text m-3 badge bg-primary text-light">
-                    {props.valor.department}</span></p>
-                    </Card.Text>
-                </div>
-            </div>                
+                <section className='row'>
+                    <div className='col-sm-12 col-md-4'>
+                        {/* columna para la imagen del personaje */}
+                        <img src={props.valor.pic} className="card-img-top" alt={props.valor.fullName}></img>
+                    </div>
+                    <div className='col-sm-12 col-md-8'>
+                        {/* columna para la frase del personaje */}
+                        <Card.Title className='my-2'>{props.valor.fullName}</Card.Title>
+                        <Card.Text className='container d-flex justify-content-between mt-3' >
+                            {props.valor.title}
+                            <Badge bg="info ms-3">{props.valor.department}</Badge>
+                        </Card.Text>
+                    </div>
+                </section>
             </Card.Body>
         </Card>
     );
